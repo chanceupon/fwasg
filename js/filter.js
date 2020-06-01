0d588adc16dd34becad9a74d00ca8d1db5a62647
Nhan <lynhan15041997@gmail.com>
2020-06-01 17:52:05 +0700

    update code

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
+  var allItem = $('.c-list__item');
+
   $('.c-list-filter__item').on('click', function() {
     $('.c-list-filter__item').removeClass('c-list-filter__item--active')
     $(this).addClass('c-list-filter__item--active')
     var data = new RegExp($(this).data('range'), 'g')
-    console.log(data)
-    $('.c-list__item').css('display', 'flex')
+    $('.c-list').empty();
 
-    $('.c-list__item').find('.c-list__item-name').each(function() {
-      console.log(!$(this).text()[0].match());
-      console.log($(this).text())
-      if (!$(this).text()[0].match(data)) {
-        $(this).parent().css('display', 'none');
+    allItem.find('.c-list__item-name').each(function() {
+      if ($(this).text()[0].match(data)) {
+        $('.c-list').append($(this).parent())
       }
     })
   })
